7e0d05dccf8d1dd81ec6b29ba44f690189020df2
michal-szyba <mszyba@onet.pl>
2024-07-23 13:33:06 +0200

    defining points

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+point1 = Point(2, 3)
+print(point1.x, point1.y)
+points = [
+    Point(1, 3),
+    Point(3, 5),
+    Point(6, 11),
+    Point(8, 2),
+    Point(2, 7.3),
+    Point(11.1, 21.3),
+    Point(9, 18.6),
+    Point(7, 12.3),
+    Point(5.2, 2.7),
+    Point(9.9, 2.1)
+]
