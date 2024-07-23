6ad23da699f5ba1da475365f15d6529f44d16fe6
michal-szyba <mszyba@onet.pl>
2024-07-23 13:43:39 +0200

    new points and plot

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -7,14 +11,24 @@ class Point:
 point1 = Point(2, 3)
 print(point1.x, point1.y)
 points = [
-    Point(1, 3),
-    Point(3, 5),
-    Point(6, 11),
-    Point(8, 2),
-    Point(2, 7.3),
-    Point(11.1, 21.3),
-    Point(9, 18.6),
-    Point(7, 12.3),
-    Point(5.2, 2.7),
-    Point(9.9, 2.1)
+    Point(1, 2),
+    Point(2, 3),
+    Point(3, 4),
+    Point(4, 5),
+    Point(5, 6),
+    Point(6, 7),
+    Point(7, 8),
+    Point(8, 9),
+    Point(9, 10),
+    Point(10, 11)
 ]
+
+x_values = np.array([point.x for point in points])
+y_values = np.array([point.y for point in points])
+
+plt.scatter(x_values, y_values)
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
