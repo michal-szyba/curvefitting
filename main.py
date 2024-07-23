c6e4fdd04056a481d2069dddefb1aa73cd946a3a
michal-szyba <mszyba@onet.pl>
2024-07-23 15:10:55 +0200

    complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 
 class Point:
@@ -8,26 +9,32 @@ class Point:
         self.y = y
 
 
-point1 = Point(2, 3)
-print(point1.x, point1.y)
+def linear_function(x, a, b):
+    return a * x + b
+
+
 points = [
-    Point(1, 2),
-    Point(2, 3),
-    Point(3, 4),
-    Point(4, 5),
-    Point(5, 6),
-    Point(6, 7),
-    Point(7, 8),
-    Point(8, 9),
-    Point(9, 10),
-    Point(10, 11)
+    Point(1, 1),
+    Point(2, 2),
+    Point(3, 3),
+    Point(4, 4),
+    Point(5, 5),
+    Point(6, 6),
+    Point(7, 7),
+    Point(8, 8),
+    Point(9, 9),
+    Point(10, 10)
 ]
 
 x_values = np.array([point.x for point in points])
 y_values = np.array([point.y for point in points])
 
-plt.scatter(x_values, y_values)
+parameters, parameters_covariance = curve_fit(linear_function, x_values, y_values)
+print(parameters)
 
+plt.plot(x_values, linear_function(x_values, parameters[0], parameters[1]))
+
+plt.scatter(x_values, y_values, label="Points")
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
